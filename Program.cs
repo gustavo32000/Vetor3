ae4dc2bbd1adf62c1bc456c8df2572bafe65cd03
gugu <gustavocorrea2032@gmail.com>
2019-07-02 11:04:59 -0300

    Soma dos valores do vetor

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Vetor3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numeros = {10 , 23, 5, 45, 68, 12, 70};
+            Console.WriteLine("Qtd do vetor. " +numeros.Length);
+            int resultado = numeros[0];
+            for (int i = 1 ; i < numeros.Length ; i ++) {
+                resultado+=numeros[i];
+            }
+            Console.WriteLine("A soma é " + resultado);
         }
     }
 }
